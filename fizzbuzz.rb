6fa4f206cd25118e54ab7189fb56a9f0be611d23
Peter Meister <prmeister89@gmail.com>
2018-07-03 17:31:16 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
   if (int % 3 || 5 == 0)
-    return "FizzBuzz"
+    "FizzBuzz"
   elsif (int % 3  == 0)
-    return "Fizz"\
+    "Fizz"\
   elsif (int % 5  == 0)
-    return "Buzz"
+    "Buzz"
   else
-    return nil
+    nil
   end
 end
